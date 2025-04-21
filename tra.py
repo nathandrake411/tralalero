f51e709e31137e56c2e0857602ee4fe54d310ad5
W607-PC27 <axel>
2025-04-21 13:34:44 -0400

    cosas que faltaron

--- tra.py
+++ tra.py
@@ -1,13 +1,10 @@
 #hola soy un comentario
 
-estudiantesnombres = [ "maxi" , "richart" ]
-estudiantescurso = [ "primero a" , "cuarto c" ]
-estudiantesedad = [ 20 , 19 ]
-print(estudiantesnombres(0), estudiantescurso(0),
-      estudiantesedad(0))
-
 #entonces como se hace
 estudiantes = [
     { "nombre" : "maxi" , 'edad' : 20 , 'curso' : "2a" },
     {"nombre" : "richart", 'edad': 19, 'curso' : "18"}
 ]
+print(estudiantes)
+print('#####')
+print( estudiantes(0)())
