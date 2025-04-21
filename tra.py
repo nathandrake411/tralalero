a0707d833c6b149ae3e994a0f561693f6819495c
W607-PC27 <axel>
2025-04-21 13:30:01 -0400

    avances de la clases de arreglos

---
+++ tra.py
@@ -0,0 +1,13 @@
+#hola soy un comentario
+
+estudiantesnombres = [ "maxi" , "richart" ]
+estudiantescurso = [ "primero a" , "cuarto c" ]
+estudiantesedad = [ 20 , 19 ]
+print(estudiantesnombres(0), estudiantescurso(0),
+      estudiantesedad(0))
+
+#entonces como se hace
+estudiantes = [
+    { "nombre" : "maxi" , 'edad' : 20 , 'curso' : "2a" },
+    {"nombre" : "richart", 'edad': 19, 'curso' : "18"}
+]
